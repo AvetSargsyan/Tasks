5376ecd90a5826a5d70eb1d8db22be60130966ea
Ares <awet.sargsyan@mail.ru>
2022-08-05 16:30:11 +0400

    TaskN6

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# HomeWorkN5
-Task: Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+# HomeWorkN6
+Tasks:
+1.Given the string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
+2.Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest
+3.Get the day, month and year of the current date and output it to the console separately
--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
-//Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
-let animal = [];
-let cat = []
-animal.canMove = true ;
-console.log(animal);
-Object.assign(cat , animal );
-console.log(cat);
 
+//1.Given the string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
+let str ='ahb acb aeb aeeb adcb axeb'.match(/a[a-z]b/g);
+console.log(str);
 
+//2.Given the string '2 + 3 223 2223'. Write a regexp that finds line 2 + 3 without capturing the rest
+let numStr = '2 + 3 223 2223';
+const reg =  /2...3/;
+console.log(numStr.match(reg));
 
+//3.Get the day, month and year of the current date and output it to the console separately
+const date = new Date();
+const now = [date.getDate(),date.getMonth(),date.getFullYear()]
+console.log(now);

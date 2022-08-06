4e69b6cd9788fd46b77ba6cbd9e9710130607425
AvetSargsyan <91004418+AvetSargsyan@users.noreply.github.com>
2022-08-06 17:33:37 +0400

    Adding fix

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ console.log(str);
 
 //2.Given the string '2 + 3 223 2223'. Write a regexp that finds line 2 + 3 without capturing the rest
 let numStr = '2 + 3 223 2223';
-const reg =  /2...3/;
-console.log(numStr.match(reg));
+const reg =  /2 \+ 3/;
+const searcher = numStr.match(reg);
+console.log(searcher[0]);
+console.log(searcher.index);
 
 //3.Get the day, month and year of the current date and output it to the console separately
 const date = new Date();
-const now = [date.getDate(),date.getMonth(),date.getFullYear()]
+const now = [date.getDate(),date.getMonth()+1,date.getFullYear()]
 console.log(now);

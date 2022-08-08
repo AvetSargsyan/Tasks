b794a9a2f9d94e805e83315ea75e75444f919569
AvetSargsyan <91004418+AvetSargsyan@users.noreply.github.com>
2022-08-08 18:42:48 +0400

    Changing Arrays to Objects

--- prototypeAndInheritance.js
+++ prototypeAndInheritance.js
@@ -1,7 +1,6 @@
-"use strict";
 //Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
-let animal = [];
-let cat = [];
+let animal = {};
+let cat = {};
 animal.canMove = true;
 console.log(animal);
 cat.__proto__ = animal;

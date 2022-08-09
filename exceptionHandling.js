a3fb46497e034e19fc18b49f1aea6137ddf79171
AvetSargsyan <91004418+AvetSargsyan@users.noreply.github.com>
2022-08-09 12:24:25 +0400

    Update exceptionHandling.js

--- exceptionHandling.js
+++ exceptionHandling.js
@@ -1,5 +1,4 @@
 "use strict";
-
 //In a try catch construction, wrap the code: console.log (a), let a = 3. And display an error - ‘let must be declared before use’. When running 1/0, the error 'cannot be divided by zero'
 try {
   console.log(a);
@@ -8,8 +7,8 @@ try {
   console.log("Let must be declared before use");
 }
 try {
-  let err = new Error("Cannot be divided by zero");
-  if (1 / 0 == Infinity) {
-    console.log(err.message);
-  }
-} catch {}
+  throw new Error(1 / 0);
+} catch {
+  console.log("Can't be divided by zero");
+}
+

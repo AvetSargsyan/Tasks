04d7b4856940f150701ae9db88d7093022a6f0ff
AvetSargsyan <awet.sargsyan@mail.ru>
2022-08-09 13:40:09 +0400

    Adding  exceptionHandling

--- prototypeAndInheritance.js
+++
@@ -1,7 +0,0 @@
-//Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
-let animal = {};
-let cat = {};
-animal.canMove = true;
-console.log(animal);
-cat.__proto__ = animal;
-console.log(cat.canMove);
